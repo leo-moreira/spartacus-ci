07d869b22801f5b0083248ef60f80fa4bc614002
leonardo.moreira <leonardo.moreira@wolven.com.br>
2022-11-23 14:08:36 -0300

    Initialized Spartacus storefront

---
+++ src/app/spartacus/spartacus-configuration.module.ts
@@ -0,0 +1,42 @@
+import { NgModule } from '@angular/core';
+import { translationChunksConfig, translations } from "@spartacus/assets";
+import { FeaturesConfig, I18nConfig, OccConfig, provideConfig, SiteContextConfig } from "@spartacus/core";
+import { defaultCmsContentProviders, layoutConfig, mediaConfig } from "@spartacus/storefront";
+
+@NgModule({
+  declarations: [],
+  imports: [],
+  providers: [
+    provideConfig(layoutConfig),
+    provideConfig(mediaConfig),
+    ...defaultCmsContentProviders,
+    provideConfig(<OccConfig>{
+      backend: {
+        occ: {
+          baseUrl: 'https://spartacus-demo.eastus.cloudapp.azure.com:8443/',
+        },
+      },
+    }),
+    provideConfig(<SiteContextConfig>{
+      context: {
+        urlParameters: ['baseSite', 'language', 'currency'],
+        baseSite: ['electronics-spa', 'apparel-uk-spa'],
+        language: ['en'],
+        currency: ['USD', 'GBP']
+      },
+    }),
+    provideConfig(<I18nConfig>{
+      i18n: {
+        resources: translations,
+        chunks: translationChunksConfig,
+        fallbackLang: 'en',
+      },
+    }),
+    provideConfig(<FeaturesConfig>{
+      features: {
+        level: '4.3',
+      },
+    }),
+  ],
+})
+export class SpartacusConfigurationModule {}
